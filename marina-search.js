4dceceb6453de413fc5e0ad7a814bbfc04497dc2
Gemin Pak <97317647+pakgem@users.noreply.github.com>
2022-06-22 16:16:43 -0700

    remove src links

--- marina-search.js
+++ marina-search.js
@@ -11,9 +11,6 @@
       });
   });
 
-<script src="https://cdn.jsdelivr.net/npm/algoliasearch@4.5.1/dist/algoliasearch-lite.umd.js" integrity="sha256-EXPXz4W6pQgfYY3yTpnDa3OH8/EPn16ciVsPQ/ypsjk=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.8.3/dist/instantsearch.production.min.js" integrity="sha256-LAGhRRdtVoD6RLo2qDQsU2mp+XVSciKRC8XPOBWmofM=" crossorigin="anonymous"></script>
-
 	var search = instantsearch({
     searchClient: algoliasearch('70H8CY4ZEE', 'ebb2e4d36e27f20d4e1d6b237243c8dd'),
     indexName: 'marina_profiles'
